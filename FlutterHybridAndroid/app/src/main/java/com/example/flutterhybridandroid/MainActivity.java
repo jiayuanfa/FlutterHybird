8bb614509df13f7b47bcaf600f19c467b8b360be
贾元发 <jiayuanfa@yeah.net>
2022-10-27 17:37:23 +0800

    Flutter与Android混编与调用

--- FlutterHybridAndroid/app/src/main/java/com/example/flutterhybridandroid/MainActivity.java
+++ FlutterHybridAndroid/app/src/main/java/com/example/flutterhybridandroid/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.flutterhybridandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import io.flutter.embedding.android.FlutterFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView getFlutterTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFlutterTv = findViewById(R.id.getFlutter);
+        getFlutterTv.setOnClickListener(view -> {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.frameLayout, FlutterFragment.createDefault());
+            ft.commit();
+        });
     }
 }
